16fb81b8b82cf4bdf14fa164d328c5d08bc4264b
MarcoTz <38535746+MarcoTz@users.noreply.github.com>
2022-11-04 12:02:56 +0100

    added multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,5 @@ streamlit.header('A Headline')
 streamlit.text('Lorem Ipsum dolor sit amet.')
 
 fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.multiselect('Pick some fruit',list(fruit_list.index))
 streamlit.dataframe(fruit_list)
