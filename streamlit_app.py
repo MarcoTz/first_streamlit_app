91441ed36883b7bf0e689d222ba007b40819f8d0
MarcoTz <38535746+MarcoTz@users.noreply.github.com>
2022-11-04 12:00:04 +0100

    fixed read_csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('Testing out Streamlit')
 streamlit.header('A Headline')
 streamlit.text('Lorem Ipsum dolor sit amet.')
 
-fruit_list = pandas.read('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.dataframe(fruit_list)
