28abcd61751afae7b6b6d25474ba76747355a7cb
MarcoTz <marco.tzschentke@gmail.com>
2022-11-04 12:34:02 +0100

    added default

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ streamlit.text('Lorem Ipsum dolor sit amet.')
 fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 fruit_list = fruit_list.set_index('Fruit')
 
-streamlit.multiselect('Pick some fruit',list(fruit_list.index))
+streamlit.multiselect('Pick some fruit',list(fruit_list.index),['Avocado','Strawberries')
 streamlit.dataframe(fruit_list)
