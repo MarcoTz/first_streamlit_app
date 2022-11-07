fe0487657f6bcdb8ef3e393b2727f4c497654280
MarcoTz <marco.tzschentke@gmail.com>
2022-11-07 11:11:26 +0100

    added database query

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,7 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake")
-streamlit.text(my_data_row)
+
 
 streamlit.title('Testing out Streamlit')
 streamlit.header('A Headline')
@@ -31,3 +28,8 @@ streamlit.write('The user entered', fruit_choice)
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+my_cur.execute("SELECT * FROM fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake")
+streamlit.text(my_data_row)
