ff6542089fe1db4ea89c0e80b74441d7c2b2ee92
MarcoTz <marco.tzschentke@gmail.com>
2022-11-07 11:12:44 +0100

    changed to fetch all

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.text("Hello from Snowflake")
-streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
